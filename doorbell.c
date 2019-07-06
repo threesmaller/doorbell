d1f803ff66761c0b0bc9c3b1051291336ec26860
fnick1010 <52483158+fnick1010@users.noreply.github.com>
2019-07-06 17:36:47 +0800

    fix compiler error

--- doorbell.c
+++ doorbell.c
@@ -1,45 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <fcntl.h>
+#include "doorbell.h"
 
 int GetState;
-int fd_socket; 
+int fd_socket;
+uint32_t send_data = 0;
+uint32_t send_count = 0;
+uint32_t recv_count = 0;
+uint32_t check_socket = 0;
 
 //############################################################################
 //
 //	open_socket
 //
 //############################################################################
-void open_socket();
+int open_socket()
 {
 	struct hostent *hp;
     struct sockaddr_in clientaddr;
 	struct sockaddr_in servaddr;
 	
-	if ((fd_socket = socket(AF_INET, SOCK_DGRAM, 0)) <0) 
+	if((fd_socket = socket(AF_INET, SOCK_DGRAM, 0)) <0) 
 	{
-		printf(stderr, "can't open socket device");
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "can't open socket device");
+		return -1;
     }
+	fcntl(fd_socket, F_SETFL, O_NONBLOCK);
 	
-	memset (&myaddr, 0, sizeof (myaddr)) ;
+	memset (&clientaddr, 0, sizeof (clientaddr)) ;
 	clientaddr.sin_family = AF_INET;
 	clientaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	clientaddr.sin_port = htons(0);
 	
-	if (bind(fd_socket, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) <0) 
+	if(bind(fd_socket, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) <0) 
 	{
-		printf(stderr, "bind failed");
-		exit(1);
+		fprintf(stderr, "bind failed");
+		return -1;
     }
 
 	memset (&servaddr, 0, sizeof (servaddr)) ;
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
 	hp = gethostbyname(SERV_IP);
-	servaddr.sin_addr = *((struct in_addr *)hp->h_addr);	
+	servaddr.sin_addr = *((struct in_addr *)hp->h_addr);
+	return 0;
 }
 
 //############################################################################
@@ -49,38 +60,44 @@ void open_socket();
 //############################################################################
 int update_GetState(int fd, int GetState)
 {	
-	if (ioctl(fd, GPIOCTL_IOC_RD_BUTTON, &GetState) < 0)
+	if(ioctl(fd, GPIOCTL_IOC_RD_BUTTON, &GetState) < 0)
 		return -1;
-	if( GetState == 0 )		//button release
+	if(GetState == 0)		//button release
 	{
 		send_count = 0;
 		send_data = 0;
 		check_socket = 0;
 		close(fd_socket);
-		if(ioctl(fd_pca9536, GPIOCTL_IOC_WR_LED_WHITE, 0) < 0)	//LED WHITE off
+		if(ioctl(fd, GPIOCTL_IOC_WR_LED_WHITE, 0) < 0)	//LED WHITE off
 		{
-			printf(stderr, "GPIOCTL_IOC_WR_LED_WHITE failed");
-			exit(1);						
+			fprintf(stderr, "GPIOCTL_IOC_WR_LED_WHITE failed");
+			return -1;					
 		}
+		else
+			return 0;
+		
 	}
 	else
 	{
 		char send_msg[2];
 		char buf[2];
-		int temp;
+		unsigned int temp;
+		int ret;
 		if(check_socket == 0)
 		{
 			check_socket++;
-			open_socket();
+			ret = open_socket();
+			if(ret < 0)
+				return -1;
 		}
 			
 		sprintf(send_msg,"%d",send_count);
-		if( send_data == 0 && recv_count = 0)
+		if( send_data == 0 && recv_count == 0)
 		{
 			if(send(fd_socket, send_msg, sizeof(send_count), 0) < 0)
 			{
-				printf(stderr, "send failed");
-				exit(1);						
+				fprintf(stderr, "send failed");
+				return -1;						
 			}
 			else
 				send_data = 1;
@@ -89,11 +106,11 @@ int update_GetState(int fd, int GetState)
 		{
 			if(recv(fd_socket, buf, sizeof(buf), 0) < 0)
 			{
-				printf(stderr, "send failed");
-				exit(1);
+				fprintf(stderr, "send failed");
+				return -1;
 			}
 			temp = atoi(buf);
-			if( temp == send_count )
+			if(temp == send_count)
 			{
 				send_count++;
 				send_data = 0;
@@ -101,85 +118,71 @@ int update_GetState(int fd, int GetState)
 			else
 			{
 				recv_count++;
-				if( recv_count == 3 )
+				if(recv_count == 3)
 				{
 					recv_count = 0;
 					send_data = 0;
 				}
 			}
 		}
-		
 	}
+	return 0;
 }
 int main(int argc, char **argv)
 {
     int fd_adc;
 	int fd_gpioctl;
 	
-	int	check_v = 0;
-	int send_data = 0;
-	int send_count = 0;
-	int recv_count = 0;
-	int check_socket = 0;
+	uint32_t check_v = 0;
 	
-	if ((fd_adc = open("/dev/adc", O_RDWR)) < 0) 
+	
+	if((fd_adc = open("/dev/adc", O_RDWR)) < 0) 
 	{
-    	printf(stderr, "can't open ADC device");
-		exit(1);
+    	fprintf(stderr, "can't open ADC device");
+		return -1;
     }
 	
-	if ((fd_gpioctl = open("/dev/gpioctl", O_RDWR)) < 0) 
+	if((fd_gpioctl = open("/dev/gpioctl", O_RDWR)) < 0) 
 	{
-    	printf(stderr, "can't open GPIO ctrl device");
-		exit(1);
+    	fprintf(stderr, "can't open GPIO ctrl device");
+		return -1;
     }
-	
-
-						
+				
     while(1)
     {
-        int ret;
         int data;		
 		
-        ret = read(fd_adc, &data, sizeof(data));
+        read(fd_adc, &data, sizeof(data));
 
-        if(ret != sizeof(data))
-        {
-            if(errno != EAGAIN)
-                printf("Read ADC Device Faild!\n");
-            continue;
-        }
-        else
-        {
-            if(data > 3500)		//voltage is > 3.5V
+		if(data > 3500)		//voltage is > 3.5V
+		{
+			if(update_GetState(fd_gpioctl, GetState) < 0)
 			{
-				if (update_GetState(fd_gpioctl, &GetState) < 0)
+				fprintf(stderr, "GPIOCTL_IOC_RD_BUTTON failed");
+				return -1;					
+			}	
+		}
+		else
+		{
+			if(check_v % 4 == 0)
+			{
+				if(ioctl(fd_gpioctl, GPIOCTL_IOC_WR_LED_RED, 1) < 0)	//LED RED on
 				{
-					printf(stderr, "GPIOCTL_IOC_RD_BUTTON failed");
-					exit(1);						
-				}	
+					fprintf(stderr, "GPIOCTL_IOC_WR_LED_RED failed");
+					return -1;					
+				}
 			}
 			else
 			{
-				if(check_v % 4 == 0)
+				if(ioctl(fd_gpioctl, GPIOCTL_IOC_WR_LED_RED, 0) < 0)	//LED RED off
 				{
-					if(ioctl(fd_gpioctl, GPIOCTL_IOC_WR_LED_RED, 1) < 0)	//LED RED on
-					{
-						printf(stderr, "GPIOCTL_IOC_WR_LED_RED failed");
-						exit(1);						
-					}
-				}
-				else
-				{
-					if(ioctl(fd_gpioctl, GPIOCTL_IOC_WR_LED_RED, 0) < 0)	//LED RED off
-					{
-						printf(stderr, "GPIOCTL_IOC_WR_LED_RED failed");
-						exit(1);						
-					}					
-				}
-				check_v++;
+					fprintf(stderr, "GPIOCTL_IOC_WR_LED_RED failed");
+					return -1;					
+				}					
 			}
-        }
+			check_v++;
+		}
+
 		if( check_v > 0 )
 			check_v = check_v % 4;
 		usleep(125000);
--- doorbell.h
+++ doorbell.h
@@ -7,17 +7,17 @@
 
 #define SPRING_LOADED_BUTTON		GPIO(10)
 
-#define LED_WHITE					GPIO(11)
-#define LED_RED						GPIO(12)
+#define LED_WHITE                   GPIO(11)
+#define LED_RED                     GPIO(12)
 
 /* IOCTL commands */
-#define GPIOCTL_IOC_MAGIC			'k'
+#define GPIOCTL_IOC_MAGIC           'k'
 
-#define GPIOCTL_IOC_RD_BUTTON		_IOR(GPIODRV_IOC_MAGIC, 1, long)
-#define GPIOCTL_IOC_WR_LED_WHITE	_IOW(GPIODRV_IOC_MAGIC, 2, long)
-#define GPIOCTL_IOC_WR_LED_RED		_IOW(GPIODRV_IOC_MAGIC, 3, long)
+#define GPIOCTL_IOC_RD_BUTTON		_IOR(GPIOCTL_IOC_MAGIC, 1, long)
+#define GPIOCTL_IOC_WR_LED_WHITE	_IOW(GPIOCTL_IOC_MAGIC, 2, long)
+#define GPIOCTL_IOC_WR_LED_RED		_IOW(GPIOCTL_IOC_MAGIC, 3, long)
 
-#define SERV_PORT					13469
-#define SERV_IP						"test.ring.com"
+#define SERV_PORT                   13469
+#define SERV_IP                     "test.ring.com"
 
 #endif
